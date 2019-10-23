d8b384f0b434be0bcc6542a61bbcd04a5e853ed9
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-23 15:09:13 -0700

    Add active admin.

--- Gemfile
+++ Gemfile
@@ -39,3 +39,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+# ActiveAdmin
+gem 'devise'
+gem 'activeadmin'
+
+gem 'sprockets', '~>3.0'
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,11 @@ module Cpr
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Middleware for ActiveAdmin
+    config.middleware.use Rack::MethodOverride
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
