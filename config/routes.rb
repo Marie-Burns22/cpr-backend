937d2046d489799f9503a804d2a46e4a9c84d826
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-10-23 22:47:21 -0700

    Namespaced routes and controller for courses.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  # root 'welcome#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  namespace :api do
+    namespace :v1 do
+      resources :courses
+    end
+  end
 end
